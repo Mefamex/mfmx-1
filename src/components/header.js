ab640a2bd208abc341996e1d22f290d04fca8643
Mefamex <131618733+Mefamex@users.noreply.github.com>
2024-09-13 07:54:14 +0300

    update-header

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,9 @@ function CreateHeader() {
     const header = document.querySelector("header");
     header.innerHTML = `<link rel="stylesheet" type="text/css" href="/src/components/header.css">`
 
+    const headerDivLeftSpace = document.createElement("div"); header.appendChild(headerDivLeftSpace);
+    headerDivLeftSpace.style.display = "flex"; //for divleft to center
+
     const divLeft = document.createElement("div"); header.appendChild(divLeft);
     divLeft.id = "headerDivLeft"
     divLeft.addEventListener('click', () => { window.location.href = "/" });
@@ -37,16 +40,18 @@ function CreateHeader() {
 
 
     const navBarHideButton = document.createElement("a"); header.appendChild(navBarHideButton);
-    navBarHideButton.id = "navBarHideButton"; navBarHideButton.className = "unselected"; navBarHideButton.textContent = "...";
-    navBarHideButton.onclick = () => navBarHideButton.className = navBarHideButton.className == 'unselected' ? 'selected' : 'unselected';
-    navBarHideButton.title="MENU";
+    navBarHideButton.id = "navBarHideButton"; navBarHideButton.className = "unselected";
+    navBarHideButton.title = "MENU";
+    navBarHideButton.onclick = () => {navBarHideButton.className = navBarHideButton.className == 'unselected' ? 'selected' : 'unselected';navBarHidden.className = navBarHideButton.className;}
+
 
     const navBarHidden = document.createElement("section"); header.appendChild(navBarHidden);
-    navBarHidden.id = "navBarHidden";
+    navBarHidden.id = "navBarHidden"; navBarHidden.className = navBarHideButton.className;
     navBarList.forEach((item) => {
         let temp_item = document.createElement("a"); navBarHidden.appendChild(temp_item);
         temp_item.textContent = item.text; temp_item.href = item.link;
-        temp_item.style.display="block"; temp_item.style.color="white";
+        temp_item.style.display = "block"; temp_item.style.color = "white";
     })
+    window.addEventListener('scroll', function(){navBarHideButton.className='unselected';});
 }
 
