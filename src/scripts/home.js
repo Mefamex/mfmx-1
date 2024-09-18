d1b5eea189708574af561311ba2385a13dfae5e0
Mefamex <131618733+Mefamex@users.noreply.github.com>
2024-09-18 09:30:24 +0300

    u

--- src/scripts/home.js
+++ src/scripts/home.js
@@ -1,9 +1,9 @@
 document.querySelectorAll('#divSol a').forEach((element, index) => {
-    element.style.animationDelay = (4 + index * 0.3) + "s"; // adjust the delay value as needed
+    element.style.animationDelay = (2 + index * 0.3) + "s"; // adjust the delay value as needed
 });
 
 document.querySelectorAll('main #divCardContainer .card').forEach((element, index) => {
-    element.style.animationDelay = (2 + index * 0.3) + "s"; // adjust the delay value as needed
+    element.style.animationDelay = (1 + index * 0.3) + "s"; // adjust the delay value as needed
 });
 
 
