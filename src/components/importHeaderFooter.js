8a731903c5f1a6ad15c22de41bfb15baa2241933
Mefamex <131618733+Mefamex@users.noreply.github.com>
2025-01-02 20:07:19 +0300

    u

--- src/components/importHeaderFooter.js
+++ src/components/importHeaderFooter.js
@@ -53,7 +53,7 @@ async function CreateHeader() {
     headerDivLeftSpace.id = "headerDivLeftSpace"; //for divleft to center
 
     const divLeft = document.createElement("a"); header.appendChild(divLeft);
-    divLeft.id = "headerDivLeft"; divLeft.href = "https://mefamex.com"
+    divLeft.id = "headerDivLeft"; divLeft.href = "https://mefamex.com/"
 
     const divLeftIcon = document.createElement("img"); divLeft.appendChild(divLeftIcon);
     divLeftIcon.id = "headerDivLeftIcon"
