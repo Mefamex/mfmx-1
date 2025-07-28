95ee9a06b2ffd14888e6b88f995d95d8131dd69a
Mefamex <info@mefamex.com>
2025-07-28 04:49:04 +0300

    FOOTER | new design publish

--- src/components/importHeaderFooter.js
+++ src/components/importHeaderFooter.js
@@ -1,12 +1,12 @@
 /**
  * @fileoverview MEFAMEX Header ve Footer Bileşenleri
- * @author Mefamex <info@mefamex.com>
+ * @author Mefamex <info@mefamex.com> 
  * @copyright 2024 Mefamex
  * @license MIT
  * @version 1.0.1
  * @see https://mefamex.com
  * @since 2024-08-20 
- * @lastModified 2025-07-27-T12:00:00Z  
+ * @lastModified 2025-07-28-T01:45:00Z 
  */
 'use strict';
 
@@ -18,9 +18,6 @@ function getScriptPath() {
     console.log('⚠️ HeaderFooter Script yolu bulunamadı! Varsayılan konum kullanılıyor:', fallbackPath);
     return fallbackPath;
 };
-
-
-
 
 
 let scriptPath = getScriptPath();
@@ -44,7 +41,8 @@ let scriptPath = getScriptPath();
 })();
 
 
-
+/* for development purposes only */
+// if (window.location.hostname.includes('localhost') || window.location.hostname.includes('127.0.0.1')) { setInterval(() => window.scrollTo(0, document.body.scrollHeight), 100); window.scrollTo(0, document.body.scrollHeight); }
 
 
 
@@ -54,26 +52,92 @@ async function CreateFooter() {
     footer.id = "footer"; footer.innerHTML = '';
 
     const firstPart = document.createElement("div"); firstPart.id = "firstPart"; footer.appendChild(firstPart);
-    firstPart.innerHTML = '';
+    const icon_black_path = scriptPath.replace('components/importHeaderFooter.js', 'assets/icons/black');
+    firstPart.innerHTML = `
+        <div class="FootSection"> 
+            <details open> 
+                <summary> WEBSITE </summary> 
+                <ul> <!-- Anasayfa, hakkında, projeler, blog, galeri, iletişim, özgeçmiş, Site Bağlantı Ağacı, Site Dosya Ağacı --> 
+                    <li> <a href="https://mefamex.com" title="Anasayfa"> Anasayfa </a> </li> 
+                    <li> <a href="https://mefamex.com/about/" title="Website Hakkında"> Hakkında </a> </li> 
+                    <li> <a href="https://mefamex.com/projects/" title="Projeler"> Projeler </a> </li> 
+                    <li> <a href="https://mefamex.com/blog/" title="Blog"> Blog </a> </li> 
+                    <li> <a href="https://mefamex.com/gallery/" title="Galeri"> Galeri </a> </li> 
+                    <li> <a href="https://mefamex.com/contact/" title="İletişim"> İletişim </a> </li> 
+                    <li> <a href="https://mefamex.com/cv/" title="Özgeçmiş"> Özgeçmiş </a> </li> 
+                    <li> <a href="https://mefamex.com/linktree.html" title="Website Haritası"> Site Bağlantı Ağacı </a> </li> 
+                    <li> <a href="https://mefamex.com/filetree.html" title="Website Dosya Haritası"> Site Dosya Ağacı </a> </li> 
+                </ul> 
+            </details> 
+        </div> 
+        <div class="FootSection"> 
+            <details open> 
+                <summary> PROJELER </summary> 
+                <ul> 
+                    <li> <a href=" https://mefamex.com/projects/ " title="Projeler Anasayfa" > Projeler Anasayfa  </a> </li> 
+                    <li> <a href=" https://mefamex.com/the-mefamex-project/ " title="The Mefamex Project"> The Mefamex Project  </a> </li> 
+                    <li> <a href=" https://mefamex.com/projects/html-css-book/ " title=" Webpage Notları "> Webpage Notları </a> </li> 
+                    <li> <a href=" https://mefamex.com/projects/python-to-exe/ " title=" Python to Exe "> Python to Exe </a> </li> 
+                    <li> <a href=" https://mefamex.com/projects/image-meta-dataset/ " title=" Python Image Meta Dataset "> Python Image Meta Dataset </a> </li> 
+                    <li> <a href=" https://mefamex.com/projects/dizin-listeleme/ " title=" Python Dizin Listeleme "> Python Dizin Listeleme </a> </li> 
+                    <li> <a href=" https://mefamex.com/projects/kali-linux/#default-bootloader " title=" Kali Default Bootloader "> Linux - Default Bootloader </a> </li> 
+                    <li> <a href=" https://mefamex.com/projects/kali-change-boot-background/ " title=" Kali Custom Bootloader "> Linux - Arka plan Değiştirme </a> </li> 
+                </ul> 
+            </details> 
+        </div> 
+        <div class="FootSection"> 
+            <details open> 
+                <summary> İLETİŞİM </summary> 
+                <ul> 
+                    <li> <a href="https://mefamex.com/contact/" title="İletişim"> İletişim Anasayfa </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="mailto:info@mefamex.com" title="e-mail"> <img src="${icon_black_path}/mail_icon-1.png"  alt="email icon" title="e-mail" decoding="async" loading="lazy"> E-mail: info@mefamex.com </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="https://docs.google.com/forms/d/e/1FAIpQLSeT3DIqUkKNvyjoh8n-qtXphr44jCxf9sdIizKvNkK09i4Fsw/viewform?usp=sf_link" title=" İletişim Formu "> <img src="${icon_black_path}/blog_icon.png" alt="iletisim icon" title="Iletişim Formu" decoding="async" loading="lazy"> Anonim İletişim Formu </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="https://github.com/Mefamex/" title="GitHub"> <img src="${icon_black_path}/github_icon.png" alt="GitHub icon" title="GitHub" decoding="async" loading="lazy"> GitHub </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Mefamex" title="Twitter"> <img src="${icon_black_path}/x_icon.png" alt="Twitter icon" title="Twitter" decoding="async" loading="lazy"> Twitter </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/mefamex" title="LinkedIn"> <img src="${icon_black_path}/linkedin_icon.png" alt="LinkedIn icon" title="LinkedIn" decoding="async" loading="lazy"> LinkedIn </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/mefamex" title="Instagram"> <img src="${icon_black_path}/instagram_icon.png" alt="Instagram icon" title="Instagram" decoding="async" loading="lazy"> Instagram </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/mefamex58/" title="Facebook"> <img src="${icon_black_path}/facebook_icon.png" alt="Facebook icon" title="Facebook" decoding="async" loading="lazy"> Facebook </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/@mefamex/" title="YouTube"> <img src="${icon_black_path}/youtube_icon.png" alt="YouTube icon" title="YouTube" decoding="async" loading="lazy"> YouTube </a> </li>
+                    <li> <a target="_blank" rel="noopener noreferrer" href="https://www.reddit.com/user/mefamex" title="Reddit"> <img src="${icon_black_path}/reddit_icon.png" alt="Reddit icon" title="Reddit" decoding="async" loading="lazy"> Reddit </a> </li>
+                </ul> 
+            </details> 
+        </div> 
+        <div class="FootSection"> 
+            <details close> 
+                <summary> BLOG </summary> 
+                <ul> 
+                    <li> <a href="https://mefamex.com/blog/" title="Blog"> Blog Anasayfa </a> </li>
+                    <li> <a href="#" title="#"> ------------ </a> </li> 
+                    <li> <a href="#" title="#"> Biraz </a> </li> 
+                    <li> <a href="#" title="#"> Keyfim </a> </li> 
+                    <li> <a href="#" title="#"> Yetsin </a> </li> 
+                    <li> <a href="#" title="#"> Ekliyecem </a> </li> 
+                </ul> 
+            </details> 
+        </div> 
+    `;
+    if (window.innerWidth < 40 * (parseFloat(getComputedStyle(document.documentElement).fontSize) || 16)) firstPart.querySelectorAll('details:not(:first-of-type)').forEach(details => details.removeAttribute('open'));
+    
+    const splitter = document.createElement("div"); splitter.id = "splitter"; footer.appendChild(splitter);
 
     const secondPart = document.createElement("div"); secondPart.id = "secondPart"; footer.appendChild(secondPart);
     secondPart.innerHTML = `
-        <div id='secondLeft'>
-            <a href="https://mefamex.com" title="Anasayfa"> <img src="${scriptPath.replace('importHeaderFooter.js', 'mefamex_logo_bgb.png')}" alt="mefamex_logo" id="footerLogo">  </a>
-            <span class="vl"></span>
-            <span> 2014-&infin; &copy;<b> MEFAMEX </b></span>
-            <span class="vl"></span>
-            <a href="https://mefamex.com/sitemap.xml" title="Website Haritası"> Sitemap </a>
-        </div>
+        <div id='secondLeft'> 
+            <a href="https://mefamex.com" title="Anasayfa"> <img src="${scriptPath.replace('importHeaderFooter.js', 'mefamex_logo_bgb.png')}" alt="mefamex_logo" id="footerLogo"> </a> 
+            <span class="vl"> </span> 
+            <span> 2014-&infin; &copy;<b> MEFAMEX </b> </span> 
+            <span class="vl"> </span> 
+            <a href="https://mefamex.com/sitemap.xml" title="Website Haritası"> Sitemap  </a> 
+        </div> 
         <div id='secondRight'> 
-            <a href="https://github.com/Mefamex" target="_blank" rel="noopener noreferrer" title="GitHub"><img src="${scriptPath.replace('components/importHeaderFooter.js', 'assets/icons/black/github_icon.png')}" alt="GitHub"></a>
-            <span class="vl"></span>
-            <a href="https://www.instagram.com/mefamex" target="_blank" rel="noopener noreferrer" title="Instagram"><img src="${scriptPath.replace('components/importHeaderFooter.js', 'assets/icons/black/instagram_icon.png')}" alt="Instagram"></a>
-            <span class="vl"></span>
-            <a href="https://www.linkedin.com/in/mefamex/" target="_blank" rel="noopener noreferrer" title="LinkedIn"><img src="${scriptPath.replace('components/importHeaderFooter.js', 'assets/icons/black/linkedin_icon.png')}" alt="LinkedIn"></a>
-        </div>
+            <a href="https://github.com/Mefamex" target="_blank" rel="noopener noreferrer" title="GitHub"> <img src="${scriptPath.replace('components/importHeaderFooter.js', 'assets/icons/black/github_icon.png')}" alt="GitHub"> </a> 
+            <span class="vl"> </span> 
+            <a href="https://www.instagram.com/mefamex" target="_blank" rel="noopener noreferrer" title="Instagram"> <img src="${scriptPath.replace('components/importHeaderFooter.js', 'assets/icons/black/instagram_icon.png')}" alt="Instagram"> </a> 
+            <span class="vl"> </span> 
+            <a href="https://www.linkedin.com/in/mefamex/" target="_blank" rel="noopener noreferrer" title="LinkedIn"> <img src="${scriptPath.replace('components/importHeaderFooter.js', 'assets/icons/black/linkedin_icon.png')}" alt="LinkedIn"> </a> 
+        </div> 
     `;
-    footer.querySelectorAll("a").forEach((link) => {
+    secondPart.querySelectorAll("a").forEach((link) => {
         const nSpan = document.createElement("span");
         nSpan.className = "AlinkPopup";
         nSpan.textContent = link.title || link.textContent || link.href;
@@ -81,9 +145,37 @@ async function CreateFooter() {
     });
 
     if (footer !== document.body.lastChild) { document.body.appendChild(footer); }
+
+    await new Promise(resolve => setTimeout(resolve, 200));
+
     //footer.scrollIntoView({ behavior: 'smooth', block: 'start' });
     //document.querySelector("main").style.maxHeight = '1rem';
     //document.querySelector("main").style.minHeight = '50vh';
+
+
+    const styleElement = document.createElement('style');
+    styleElement.id = 'for_footer_visibility_and_animation';
+    styleElement.textContent = ` .footer-hidden { opacity: 0; transform: translateY(2em); transition: all 1s ease-out; } .footer-visible { opacity: 1; transform: translateY(0); } `;
+    document.head.appendChild(styleElement);
+    if (firstPart) firstPart.querySelectorAll('details').forEach(details => { details.querySelectorAll('a').forEach(li => { li.classList.add('footer-hidden'); }); });
+    if (secondPart) { Array.from(secondPart.children).forEach(childd => { if (childd.nodeType === Node.ELEMENT_NODE) { Array.from(childd.children).forEach(child => { if (child.nodeType === Node.ELEMENT_NODE) child.classList.add('footer-hidden'); }); } }); }
+    const observerOptions = { root: null, rootMargin: '0px', threshold: 0.2 };
+    if (firstPart) { firstPart.querySelectorAll('details').forEach(details => { const detailsObserver = new IntersectionObserver((entries, observer) => { entries.forEach(entry => { if (entry.isIntersecting) { const listItems = entry.target.querySelectorAll('a'); listItems.forEach((li, index) => { setTimeout(() => { li.classList.remove('footer-hidden'); li.classList.add('footer-visible'); }, index * (Math.floor(Math.random() * 50) + 100)); }); observer.unobserve(entry.target); } }); }, observerOptions); detailsObserver.observe(details); }); }
+    if (secondPart) {
+        requestAnimationFrame(() => {
+            const secondPartObserver = new IntersectionObserver((entries, observer) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        const children = Array.from(entry.target.children).filter(child => child.nodeType === Node.ELEMENT_NODE);
+                        children.forEach((childd) => { Array.from(childd.children).forEach((child, index) => { setTimeout(() => { child.classList.remove('footer-hidden'); child.classList.add('footer-visible'); }, index * (Math.floor(Math.random() * 101) + 200)); }); });
+                        setTimeout(() => { const styleElement = document.getElementById('for_footer_visibility_and_animation'); if (styleElement) styleElement.remove(); footer.querySelectorAll('.footer-hidden, .footer-visible').forEach(element => { element.classList.remove('footer-hidden', 'footer-visible'); }); }, 10000);
+                        observer.unobserve(entry.target);
+                    }
+                });
+            }, observerOptions);
+            secondPartObserver.observe(secondPart);
+        });
+    }
 }
 
 
